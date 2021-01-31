0dae83f76008a6efb40b4e4ec51033b1e6969f04
Vinicius Fritzen Machado <fritzenvinicius@gmail.com>
2021-01-31 10:29:10 -0300

    Improvements on Header - Allow back to home and perfil screen

--- aconteceosorioweb/src/pages/componentsPage/HeaderPage.js
+++ aconteceosorioweb/src/pages/componentsPage/HeaderPage.js
@@ -9,23 +9,30 @@ export default function HeaderPage (props) {
             <nav class="navbar navbar-expand-lg navbar-light bg-secondary shadow-sm">
                 
                 <div class="container-fluid" >
-                    <img style={{width:'80px'}} src='images/logoAcontece.png' alt='Acontece Osório - Logotipo' />
+                    <Link to='/'>
+                        <img style={{width:'80px'}} src='images/logoAcontece.png' alt='Acontece Osório - Logotipo' />
+                        <h2 class="navbar-brand text-white ml-5">
+                            INÍCIO
+                        </h2>
+                    </Link>
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
+                        
                     </button>
 
-                    {props.name !== '' && 
-                    <h2 class="text-white">{props.name}</h2>
-                    }
-
-                    <div class="collapse navbar-collapse" id="navbarToggler">
-                        <hr/>
-                       
+                   <div class="collapse navbar-collapse" id="navbarToggler">
                         
                         <div class="navbar-nav ml-auto">
-                            <Link to={props.urlPath}>
+
+                            <Link to='InitialUserPage'>
                                 <h2 class="navbar-brand text-white">
+                                {props.name} - PERFIL
+                                </h2>
+                            </Link>
+
+                            <Link to={props.urlPath}>
+                                <h2 class="navbar-brand text-white ml-4">
                                     {props.textButton}
                                 </h2>
                             </Link>
