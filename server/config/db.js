d66df416fa5bc1cb08c4ef8b0fc61b671a4f925e
Vinicius Machado <fritzenvinicius@gmail.com>
2021-01-30 22:32:29 -0300

    Update dev database to a remote server

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,21 @@ const mongoose =  require('mongoose')
 
 const userSchema = require('../models/userSchema')
 
+console.log("Connecting to DB...")
 mongoose.connect(
-    "mongodb://localhost/acontece-osorio",
+    'mongodb+srv://acontece-osorio:acontece-osorio-dev@cluster0.gn7yp.mongodb.net/acontece-osorio?retryWrites=true&w=majority',
     {
         useNewUrlParser: true, 
         useFindAndModify: false, 
         useCreateIndex: true, 
         useUnifiedTopology: true}
-);
+)
+.then(ok => {
+    console.log("Connected!");
+})
+.catch(error => {
+    console.log(error);
+});
 
 const user = mongoose.model('user', userSchema)
 
